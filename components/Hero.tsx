fb28e0376efa1dff4e99c0042d97f90d4568ac92
lyncheng <lyncheng@fs-technology.com>
2024-04-08 17:12:46 +0800

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">Find,Book or rent car</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
